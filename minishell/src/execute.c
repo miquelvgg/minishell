70e815839e33d6848ce3d9de98808b8f0fb2ebb6
Eloy Antoni Pascual Perez-Casaux <epascual@car1s6.42barcelona.com>
2025-11-21 19:24:06 +0100

    Eliminado comentario innecesario.

--- minishell/src/execute.c
+++ minishell/src/execute.c
@@ -1,42 +1,6 @@
 #include "minishell.h"
 
-/*void	execute(t_data *minishell)
-{
-	printf("%s\n",minishell->token->data);
-	char	*path;
-	int		excode;
-	char	**s_cmd;
-	int		pid;
-
-	excode = 0;
-	s_cmd = ft_split(minishell->token->data, ' ');
-	if (is_builtin(s_cmd[0]))
-			execute_builtin(s_cmd, minishell);
-	else
-	{
-		pid = fork();
-		if (pid == 0)
-		{
-			printf("Path");
-			path = get_path(s_cmd[0], minishell->env);
-			printf(" obtenido\n");
-			excode = execve(path, s_cmd, NULL);
-			printf("\npostejecutado\n");
-		}
-		else
-		{
-			printf("esperando\n");
-			waitpid(pid, NULL, 0);
-			printf("esperado\n");
-		}
-	}
-	printf("liberando");
-	//free(s_cmd);
-	if (excode != 0)
-	{
-		ft_free_pointstring(s_cmd);
-	}
-}*/
+//executa un comando
 void execute(t_data *minishell)
 {
     printf("%s\n", minishell->token->data);
@@ -57,9 +21,7 @@ void execute(t_data *minishell)
         pid = fork();
         if (pid == 0)
         {
-            printf("Path");
             path = get_path(s_cmd[0], minishell->env);
-            printf(" obtenido\n");
             excode = execve(path, s_cmd, NULL);
             if (excode == -1)
             {
@@ -70,9 +32,7 @@ void execute(t_data *minishell)
         }
         else
         {
-            printf("esperando\n");
             waitpid(pid, NULL, 0);
-            printf("esperado\n");
             ft_free_pointstring(s_cmd);
         }
     }
